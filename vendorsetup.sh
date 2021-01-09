bbb86c841ee127aaeb473c937c968b9c669ccc29
lehmancurtis147 <lehmancurtis147@gmail.com>
2021-01-08 21:19:48 -0600

    vendorsetup.sh edit

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+#!/use/bin/env bash
 #
 # Copyright (C) 2017 The Android Open Source Project
 # Copyright (C) 2017 The TWRP Open Source Project
@@ -15,5 +16,20 @@
 # limitations under the License.
 #
 
+TARGET_DEVICE="simba6_cricket"
+export TARGET_DEVICE="simba6_cricket"
+
 add_lunch_combo omni_simba6_cricket-userdebug
 add_lunch_combo omni_simba6_cricket-eng
+
+export LC_ALL="C" \
+    ARCH="arm" \
+    ALLOW_MISSING_DEPENDENCIES="true" \
+    BUILD_TYPE="Official" \
+    TARGET_DEVICE="simba6_cricket" \
+    TARGET_DEVICE_ALT="simba6_cricket" \
+    TW_MAINTAINER="lehmancurtis147"
+
+if [[ "$TARGET_DEVICE" == simba6 ]]; then
+    export TARGET_DEVICE="simba6_cricket"
+fi
